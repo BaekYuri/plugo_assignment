93dafd32cd27903248604e3ba366c63311798a83
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-05-30 06:33:38 +0900

    feat : 폴더 구조 설정

---
+++ src/pages/cart/CartList.jsx
@@ -0,0 +1,4 @@
+function CartList() {
+  return <div>카트리스트</div>;
+}
+export default CartList;
---
+++ src/pages/product/ProductList.jsx
@@ -0,0 +1,4 @@
+function ProductList() {
+  return <div>프로덕트리스트</div>;
+}
+export default ProductList;

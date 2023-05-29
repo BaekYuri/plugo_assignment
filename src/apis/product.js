4979c390d7bec886ea779f141901725838d3f020
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-05-30 08:24:50 +0900

    feat : getProductById api 추가

--- src/apis/product.js
+++ src/apis/product.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export const getProduct = async () => {
-  return await axios.get("./data/mock.json");
+export const getProductAll = async () => {
+  return await axios.get("/data/mock.json");
+};
+
+export const getProductById = async (id) => {
+  return await axios.get(`/data/mock_id${id.queryKey[1]}.json`);
 };

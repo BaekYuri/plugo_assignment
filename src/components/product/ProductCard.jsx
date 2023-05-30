875d541686fede31d931a7ddd70bdd235e135b01
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-05-30 09:25:02 +0900

    feat : 장바구니 버튼 추가

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,17 +1,34 @@
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+} from "@mui/material";
+import { addCart } from "../../utils/cart";
 function ProductCard(props) {
   const { product } = props;
+
+  const cartButtonHandler = () => {
+    addCart(product, 1);
+  };
   return (
     <Card>
       <CardMedia component="img" image={product.image} alt="Paella dish" />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h6" component="div">
           {product.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.price}
         </Typography>
       </CardContent>
+      <CardActions>
+        <Button size="small" onClick={cartButtonHandler}>
+          장바구니
+        </Button>
+      </CardActions>
     </Card>
   );
 }

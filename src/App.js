62f5248ea02b5585c840a81b46c55564a8e85048
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-05-30 12:32:08 +0900

    feat : 라우터 props 설정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/ui/header/Header";
 import CartList from "./pages/cart/CartList";
 import ProductList from "./pages/product/ProductList";
 import ProductDetail from "./pages/product/ProductDetail";
 import "./App.css";
+import { getCart } from "./utils/cart";
 
 function App() {
+  const [cart, setCart] = useState(getCart());
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/product" Component={ProductList} />
-        <Route path="/cart" Component={CartList} />
-        <Route path="/product/:id" Component={ProductDetail} />
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/product" element={<ProductList setCart={setCart} />} />
+        <Route
+          path="/product/:id"
+          element={<ProductDetail setCart={setCart} />}
+        />
+        <Route
+          path="/cart"
+          element={<CartList cart={cart} setCart={setCart} />}
+        />
       </Routes>
     </BrowserRouter>
   );
